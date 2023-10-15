d52da2e92d6b945487da65f6c8f997d5acc34a18
MSI <bedbabisa@gmail.com>
2023-10-15 14:40:38 +0100

    git

--- src/client/Client.java
+++ src/client/Client.java
@@ -10,21 +10,29 @@ public class Client {
     public static void main(String[] args){
         try
         {
-
+            // Définir l'adresse IP du serveur
             InetAddress IA = InetAddress.getByName("10.26.14.146");
+
+            // Définir l'adresse IP et le port du serveur
             InetSocketAddress ISA = new InetSocketAddress(IA,1234);
 
+            // Créer un socket client et se connecter au serveur
             Socket client = new Socket();
             client.connect(ISA);
 
+            // Initialiser les flux d'entrée et de sortie pour communiquer avec le serveur
             InputStream input = client.getInputStream();
             OutputStream output = client.getOutputStream();
             InputStreamReader isr = new InputStreamReader(input);
             BufferedReader br = new BufferedReader(isr);
 
+            // Afficher un message de confirmation de la connexion
             System.out.println("connected");
+
+            // Créer un scanner pour obtenir les entrées de l'utilisateur
             Scanner scanner = new Scanner(System.in);
 
+            // Demander à l'utilisateur d'entrer deux entiers
             System.out.print("Please enter an integer1: ");
             String nb1 = scanner.nextLine();
             System.out.print("Please enter an integer2: ");
@@ -32,22 +40,25 @@ public class Client {
 
             String op ;
             do{
-                System.out.print("Please enter an operator: ");
+                // Demander à l'utilisateur d'entrer un opérateur valide
+                System.out.print("Please enter an operator (+, -, *, /): ");
                 op = scanner.nextLine();
             }while (!(op.equals("+")) && !(op.equals("-")) && !(op.equals("*")) && !(op.equals("/")));
 
+            // Initialiser un PrintWriter pour envoyer les données au serveur
             PrintWriter pw = new PrintWriter(output, true);
 
+            // Envoyer les nombres et l'opérateur au serveur
             pw.println(nb1);
             pw.println(nb2);
             pw.println(op);
 
-
-
-            System.out.println("Recieved result from server = "+ br.readLine());
+            // Recevoir et afficher la réponse du serveur
+            System.out.println("Received result from server = "+ br.readLine());
 
         }catch(Exception e){
-            System.out.println("CLient here");
+            // Gestion des erreurs
+            System.out.println("Client here");
             throw new RuntimeException(e);
         }
     }
--- src/server/Server.java
+++ src/server/Server.java
@@ -9,19 +9,27 @@ import java.net.Socket;
 
 public class Server {
     public static void  main(String[] args){
+        // Le serveur commence ici
         try(ServerSocket ss = new ServerSocket(1234))
         {
+            // Attendez qu'un client se connecte
             Socket clientSocket = ss.accept();
+
+            // Initialisation des flux d'entrée et de sortie
             InputStream input = clientSocket.getInputStream();
             OutputStream output = clientSocket.getOutputStream();
 
+            // Préparez les outils pour lire les données du client
             InputStreamReader isr = new InputStreamReader(input);
             BufferedReader br = new BufferedReader(isr);
 
+            // Lire les numéros et l'opération envoyés par le client
             int nb1 = Integer.parseInt(br.readLine());
             int nb2 = Integer.parseInt(br.readLine());
             String op = br.readLine();
             int res= 0;
+
+            // Effectuer l'opération appropriée en fonction de l'opérateur
             switch (op){
                 case "+" : res = nb1 + nb2 ;break;
                 case "-" : res = nb1 - nb2 ;break;
@@ -29,10 +37,12 @@ public class Server {
                 case "/" : res = nb1 / nb2 ;break;
             }
 
+            // Préparez un outil pour écrire la réponse au client
             PrintWriter pw = new PrintWriter(output, true);
             pw.println(res);
 
-        }catch (IOException e) {
+        } catch (IOException e) {
+            // Gestion des erreurs
             System.out.println("here");
             throw new RuntimeException(e);
         } ;
